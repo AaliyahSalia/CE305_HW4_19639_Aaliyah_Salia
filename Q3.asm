79bd1c99aacdc5901af6e6493a4371bc5d1fa599
AaliyahSalia <101353473+AaliyahSalia@users.noreply.github.com>
2023-11-18 17:59:58 -0800

    Create Q3.asm

---
+++ Q3.asm
@@ -0,0 +1,14 @@
+Begin, Load X /Load value at X
+    Output    /Print X
+    Add one   /Increment X by 1
+    Store X   /Store the incremented value at X
+    Subt ten  
+    Skipcond 400 
+    Jump Begin   
+    Halt     
+    
+/Variable declaration
+X, DEC 0
+ten, DEC 5
+one, DEC 1
+
